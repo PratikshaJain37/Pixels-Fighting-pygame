73749ee85e655e5dd9607a56c5e98306c82f5f3e
PratikshaJain37 <pratikshajain123@gmail.com>
2020-07-26 10:47:46 +0530

    v1.4: Updated logic in helpers.py

--- helpers.py
+++ helpers.py
@@ -13,20 +13,25 @@ def UpdateArray(current_status, INT):
     updated_status = np.zeros((INT,INT), dtype=int)
     for i in range(0,INT):
         for j in range(0,INT):
-            updated_status[i][j] = GetStatus(GetAliveRatio(i,j,current_status,INT))
+            status, ratio = GetRatio(i,j,current_status,INT)
+            updated_status[i][j] = GetStatus(status, ratio)
     
     return updated_status
 
 # For calculating status of individual box
-def GetStatus(alive_ratio):
+def GetStatus(status,ratio):
     random_number = random.random()
-    if random_number <= alive_ratio:
-        return 1
+    if random_number >= ratio:
+        flip = False
     else:
-        return 0
+        flip = True
+    if flip == True:
+        return status
+    else:
+        return 1 - status
 
 # For calculating alive ratio of individual box
-def GetAliveRatio(x,y,current_status_array,INT):
+def GetRatio(x,y,current_status_array,INT):
     around_alive = 0
     around_total = 0
     for i in range(x-1,x+2):
@@ -41,7 +46,9 @@ def GetAliveRatio(x,y,current_status_array,INT):
             if current_status_array[x][j] == 1:
                 around_alive += 1
 
-
-    return around_alive/around_total
+    if current_status_array[x][y] == 0:
+        return 0, 1 - around_alive/around_total
+    else:
+        return 1, around_alive/around_total
 
 # ---------------------#
